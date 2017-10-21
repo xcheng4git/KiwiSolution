1cdae181ccb8e64c6ed8c7592d49ca2bc7e70cb1
Shawn Cheng <shawncheng@163.com>
2017-10-21 21:15:59 +0800

    增加Form11的类

---
+++ KiwiSolution/KiwiSdiExec/PersonalForm11.h
@@ -0,0 +1,36 @@
+#pragma once
+
+
+
+// CPersonalForm11 窗体视图
+
+class CPersonalForm11 : public CFormView
+{
+	DECLARE_DYNCREATE(CPersonalForm11)
+
+protected:
+	CPersonalForm11();           // 动态创建所使用的受保护的构造函数
+	virtual ~CPersonalForm11();
+
+public:
+	enum { IDD = IDD_PERSONAL_FORM11 };
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+public:
+	CString m_strCurrentFile;
+	CString m_strCurrentFolder;
+	void SetCurrentFile(CString filePath);
+	CFont m_fontEdit;
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+};
+
+
