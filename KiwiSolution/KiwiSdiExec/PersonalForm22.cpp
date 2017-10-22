75da01ed5dd76c8b3606b37ee75621f1e90d1481
Shawn Cheng <shawncheng@163.com>
2017-10-22 23:01:28 +0800

    实现了Form22数据的保存

--- KiwiSolution/KiwiSdiExec/PersonalForm22.cpp
+++ KiwiSolution/KiwiSdiExec/PersonalForm22.cpp
@@ -85,6 +85,48 @@ void CPersonalForm22::OnBnClickedCmdSaveForm()
 
 	CString strText;
 
+	int Parameters[16][6] = { { IDC_EDIT264, IDC_EDIT248, IDC_EDIT236, IDC_EDIT237, IDC_DATETIMEPICKER25, IDC_EDIT259  },
+	{ IDC_EDIT253, IDC_EDIT238, IDC_EDIT260, IDC_EDIT226, IDC_DATETIMEPICKER27, IDC_EDIT249 }, 
+	{ IDC_EDIT254, IDC_EDIT239, IDC_EDIT261, IDC_EDIT227, IDC_DATETIMEPICKER28, IDC_EDIT250 },
+	{ IDC_EDIT255, IDC_EDIT240, IDC_EDIT262, IDC_EDIT228, IDC_DATETIMEPICKER28, IDC_EDIT251 },
+	{ IDC_EDIT256, IDC_EDIT241, IDC_EDIT263, IDC_EDIT229, IDC_DATETIMEPICKER30, IDC_EDIT252 },
+	{ IDC_EDIT267, IDC_EDIT274, IDC_EDIT302, IDC_EDIT308, IDC_DATETIMEPICKER31, IDC_EDIT318 },
+	{ IDC_EDIT257, IDC_EDIT242, IDC_EDIT279, IDC_EDIT230, IDC_DATETIMEPICKER32, IDC_EDIT314 },
+	{ IDC_EDIT258, IDC_EDIT243, IDC_EDIT280, IDC_EDIT231, IDC_DATETIMEPICKER33, IDC_EDIT315 },
+	{ IDC_EDIT265, IDC_EDIT244, IDC_EDIT281, IDC_EDIT232, IDC_DATETIMEPICKER34, IDC_EDIT316 },
+	{ IDC_EDIT266, IDC_EDIT245, IDC_EDIT282, IDC_EDIT233, IDC_DATETIMEPICKER34, IDC_EDIT317 },
+	{ IDC_EDIT272, IDC_EDIT277, IDC_EDIT303, IDC_EDIT334, IDC_DATETIMEPICKER36, IDC_EDIT323 },
+	{ IDC_EDIT268, IDC_EDIT246, IDC_EDIT284, IDC_EDIT313, IDC_DATETIMEPICKER37, IDC_EDIT319 },
+	{ IDC_EDIT269, IDC_EDIT247, IDC_EDIT304, IDC_EDIT309, IDC_DATETIMEPICKER37, IDC_EDIT320 },
+	{ IDC_EDIT270, IDC_EDIT275, IDC_EDIT305, IDC_EDIT310, IDC_DATETIMEPICKER39, IDC_EDIT321 },
+	{ IDC_EDIT271, IDC_EDIT276, IDC_EDIT306, IDC_EDIT311, IDC_DATETIMEPICKER40, IDC_EDIT322 },
+	{ IDC_EDIT273, IDC_EDIT278, IDC_EDIT307, IDC_EDIT312, IDC_DATETIMEPICKER41, IDC_EDIT324 }};
+
+	for (int i = 0; i < 16; i++) {
+		GetDlgItem(Parameters[i][0])->GetWindowTextW(strText); strText.Trim();
+		if (strText.IsEmpty()) {
+			strText.ReleaseBuffer();
+			break;
+		}
+
+		ss << "insert into file_form_28 values(" << file_id << ",";
+		ss << wcstod(strText.GetBuffer(), NULL) << ","; strText.ReleaseBuffer();
+
+		GetDlgItem(Parameters[i][1])->GetWindowTextW(strText); strText.Trim();
+		ss << "'" << CW2A(strText.GetBuffer(), CP_UTF8) << "',"; strText.ReleaseBuffer();
+		GetDlgItem(Parameters[i][2])->GetWindowTextW(strText); strText.Trim();
+		ss << wcstod(strText.GetBuffer(), NULL) << ","; strText.ReleaseBuffer();
+		GetDlgItem(Parameters[i][3])->GetWindowTextW(strText); strText.Trim();
+		ss << wcstod(strText.GetBuffer(), NULL) << ","; strText.ReleaseBuffer();
+
+		GetDlgItem(Parameters[i][4])->GetWindowTextW(strText); strText.Trim();
+		ss << "'" << CW2A(strText.GetBuffer(), CP_UTF8) << "',"; strText.ReleaseBuffer();
+		GetDlgItem(Parameters[i][5])->GetWindowTextW(strText); strText.Trim();
+		ss << "'" << strText << "')"; strText.ReleaseBuffer();
+
+		help->execSQL(ss.str().c_str());
+		ss.str(""); ss.clear();
+	}
 
 FillComplete:
 	help->closeDB(); delete help;
