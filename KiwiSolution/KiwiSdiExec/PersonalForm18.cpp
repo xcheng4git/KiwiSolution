538e919575372b5c77c7882924cc2abc81d2a040
Shawn Cheng <shawncheng@163.com>
2017-10-22 19:49:45 +0800

    增加Form18的类

---
+++ KiwiSolution/KiwiSdiExec/PersonalForm18.cpp
@@ -0,0 +1,140 @@
+// PersonalForm18.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "KiwiSdiExec.h"
+#include "PersonalForm18.h"
+#include <sstream>
+using namespace std;
+#include "Utility.h"
+#include "SQLiteHelper.h"
+#include "msword/msword.h"
+#include "MainFrm.h"
+
+// CPersonalForm18
+
+IMPLEMENT_DYNCREATE(CPersonalForm18, CFormView)
+
+CPersonalForm18::CPersonalForm18()
+: CFormView(CPersonalForm18::IDD)
+{
+	LOGFONT lf; memset(&lf, 0, sizeof(LOGFONT)); lf.lfHeight = 25;  _tcsncpy_s(lf.lfFaceName, LF_FACESIZE, _T("仿宋体"), 3); lf.lfWeight = 400;
+	m_fontEdit.CreateFontIndirect(&lf);
+}
+
+CPersonalForm18::~CPersonalForm18()
+{
+	m_fontEdit.DeleteObject();
+}
+
+void CPersonalForm18::SetCurrentFile(CString filePath)
+{
+	m_strCurrentFolder = filePath.Left(filePath.Find(_T("/"), 0));
+	m_strCurrentFile = filePath.Right(filePath.GetLength() - filePath.Find(_T("/"), 0) - 1);
+}
+
+void CPersonalForm18::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CPersonalForm18, CFormView)
+	ON_BN_CLICKED(IDC_CMD_SAVE_FORM, &CPersonalForm18::OnBnClickedCmdSaveForm)
+	ON_BN_CLICKED(IDC_CMD_PRINT_FORM, &CPersonalForm18::OnBnClickedCmdPrintForm)
+	ON_BN_CLICKED(IDC_BUTTON_CLOSE_FORM3, &CPersonalForm18::OnBnClickedButtonCloseForm3)
+END_MESSAGE_MAP()
+
+
+// CPersonalForm18 诊断
+
+#ifdef _DEBUG
+void CPersonalForm18::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CPersonalForm18::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// CPersonalForm18 消息处理程序
+
+
+void CPersonalForm18::OnBnClickedCmdSaveForm()
+{
+	UpdateData();
+	// TODO:  在此添加控件通知处理程序代码
+	stringstream ss;
+	ss << "select file_id from orgnization_file where file_name='" << CW2A(m_strCurrentFile.GetBuffer(), CP_UTF8) << "' and folder_name='" <<
+		CW2A(m_strCurrentFolder.GetBuffer(), CP_UTF8) << "';";
+	TRACE(CA2W(ss.str().c_str(), CP_UTF8));
+
+	CSQLiteHelper *help = new CSQLiteHelper();
+	help->openDB("kiwi.db3");
+	int row, col;
+	char *eee = "i"; char **result = &eee;
+	char **re = help->rawQuery(ss.str().c_str(), &row, &col, result);
+	int file_id = atoi(re[1 * col + 0]);
+	ss.str(""); ss.clear();
+
+	ss << "select count(*) from file_form_flags where file_id=" << file_id << ";";
+	re = help->rawQuery(ss.str().c_str(), &row, &col, result);
+	int hasRecord = atoi(re[1 * col + 0]);
+	if (!hasRecord) {
+		ss << "insert into file_form_flags(file_id) values(" << file_id << ")";
+		help->execSQL(ss.str().c_str());
+		ss.str(""); ss.clear();
+	}
+
+	CString strText;
+#pragma region FillForm18_1
+FillForm18_1 :
+	GetDlgItem(IDC_EDIT236)->GetWindowTextW(strText); strText.Trim();
+	if (!strText.IsEmpty())
+	{
+		ss << "update file_form_flags set file_24IfHaveThisSituation=0 where file_id=" << file_id;
+
+		help->execSQL(ss.str().c_str());
+		ss.str(""); ss.clear();
+		goto FillComplete;
+	}
+
+	{
+		ss << "update file_form_flags set file_24IfHaveThisSituation=1 where file_id=" << file_id;
+		help->execSQL(ss.str().c_str());
+		ss.str(""); ss.clear();
+	}
+
+#pragma endregion
+
+#pragma region FillForm18_2
+FillForm18_2 :
+
+#pragma endregion
+
+
+FillComplete :
+	help->closeDB(); delete help;
+	ss.str("");  ss.clear();
+	GetDlgItem(IDC_CMD_SAVE_FORM)->EnableWindow(FALSE);
+}
+
+
+void CPersonalForm18::OnBnClickedCmdPrintForm()
+{
+	// TODO:  在此添加控件通知处理程序代码
+}
+
+
+void CPersonalForm18::OnBnClickedButtonCloseForm3()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	CMainFrame* pWnd = (CMainFrame*)AfxGetApp()->m_pMainWnd;
+
+	::PostMessage(pWnd->m_hWnd, WM_SHOW_DEFAULT_SUMMARY, 0l, LPARAM(&m_strCurrentFolder));
+}
---
+++ KiwiSolution/KiwiSdiExec/PersonalForm18.h
@@ -0,0 +1,40 @@
+#pragma once
+
+
+
+// CPersonalForm18 窗体视图
+
+class CPersonalForm18 : public CFormView
+{
+	DECLARE_DYNCREATE(CPersonalForm18)
+
+protected:
+	CPersonalForm18();           // 动态创建所使用的受保护的构造函数
+	virtual ~CPersonalForm18();
+
+public:
+	enum { IDD = IDD_PERSONAL_FORM18 };
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+#ifndef _WIN32_WCE
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+#endif
+
+public:
+	CString m_strCurrentFile;
+	CString m_strCurrentFolder;
+	void SetCurrentFile(CString filePath);
+	CFont m_fontEdit;
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedCmdSaveForm();
+	afx_msg void OnBnClickedCmdPrintForm();
+	afx_msg void OnBnClickedButtonCloseForm3();
+};
+
+
