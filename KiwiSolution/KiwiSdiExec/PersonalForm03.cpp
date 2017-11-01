9d6ba1476032fdba7a5e14cca17d7b2f8d3a90fe
Shawn Cheng <shawncheng@163.com>
2017-11-01 20:42:42 +0800

    关闭按钮的位图显示

--- KiwiSolution/KiwiSdiExec/PersonalForm03.cpp
+++ KiwiSolution/KiwiSdiExec/PersonalForm03.cpp
@@ -22,6 +22,7 @@ PersonalForm03::PersonalForm03()
 {
 	LOGFONT lf; memset(&lf, 0, sizeof(LOGFONT)); lf.lfHeight = 15;  _tcsncpy_s(lf.lfFaceName, LF_FACESIZE, _T("·ÂËÎÌå"), 3); lf.lfWeight = 400;
 	m_fontEdit.CreateFontIndirect(&lf);
+	m_bmpClose.LoadBitmap(IDB_BITMAP_CLOSE);
 
 	m_isModify = FALSE; for (int i = 0; i < 3; i++) m_modifiedSubform[i] = -1;
 
@@ -67,6 +68,7 @@ PersonalForm03::PersonalForm03()
 PersonalForm03::~PersonalForm03()
 {
 	m_fontEdit.DeleteObject();
+	m_bmpClose.DeleteObject();
 }
 void PersonalForm03::SetCurrentFile(CString filePath)
 {
@@ -253,6 +255,9 @@ void PersonalForm03::OnInitialUpdate()
 		GetDlgItem(IDC_CMD_SAVE_FORM)->ShowWindow(SW_HIDE);
 		GetDlgItem(IDC_CMD_UPDATE_FORM)->ShowWindow(SW_SHOW);
 	}
+
+	((CButton*)GetDlgItem(IDC_BUTTON_CLOSE_FORM3))->SetBitmap(m_bmpClose);
+
 }
 
 
@@ -714,4 +719,18 @@ void PersonalForm03::OnBnClickedCmdUpdateForm()
 		*/
 		itSubformRecids++;
 	}
+
+	ss.str("");  ss.clear();
+	ss << "update personal_form_info set modify_date=";
+	CTime today = CTime::GetCurrentTime();
+	strText = today.Format("%Y-%m-%d");
+	ss << "'" << CW2A(strText.GetBuffer(), CP_UTF8) << "', ";
+	ss << " where file_id=" << file_id << " and form_serial=";
+	ss << "'" << CW2A(_T("±í2-2"), CP_UTF8) << "';";
+	//TRACE(_T("%s\n"), CA2W(ss.str().c_str(), CP_UTF8));
+	help->execSQL(ss.str().c_str());
+
+	help->closeDB();
+	delete help;
+	ss.str("");  ss.clear();
 }
