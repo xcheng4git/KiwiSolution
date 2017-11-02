01c8d5676c49baf147b80a78f73d727d46bb19f1
Shawn Cheng <shawncheng@163.com>
2017-11-02 20:46:06 +0800

    用接口类实现查、改

--- KiwiSolution/KiwiSdiExec/PersonalForm12.cpp
+++ KiwiSolution/KiwiSdiExec/PersonalForm12.cpp
@@ -19,20 +19,55 @@ CPersonalForm12::CPersonalForm12()
 : CFormView(CPersonalForm12::IDD)
 , m_Radio12_2_0(-1)
 {
-	LOGFONT lf; memset(&lf, 0, sizeof(LOGFONT)); lf.lfHeight = 25;  _tcsncpy_s(lf.lfFaceName, LF_FACESIZE, _T("仿宋体"), 3); lf.lfWeight = 400;
-	m_fontEdit.CreateFontIndirect(&lf);
+	//LOGFONT lf; memset(&lf, 0, sizeof(LOGFONT)); lf.lfHeight = 25;  _tcsncpy_s(lf.lfFaceName, LF_FACESIZE, _T("仿宋体"), 3); lf.lfWeight = 400;
+	//m_fontEdit.CreateFontIndirect(&lf);
+	m_FormID = 12;
+	int parameters1[14][4] = { { IDC_EDIT47, IDC_EDIT98, IDC_EDIT97, IDC_EDIT99 },
+	{ IDC_EDIT48, IDC_EDIT102, IDC_EDIT133, IDC_EDIT110 },
+	{ IDC_EDIT49, IDC_EDIT103, IDC_EDIT134, IDC_EDIT111 },
+	{ IDC_EDIT50, IDC_EDIT118, IDC_EDIT135, IDC_EDIT150 },
+	{ IDC_EDIT51, IDC_EDIT119, IDC_EDIT136, IDC_EDIT151 },
+	{ IDC_EDIT52, IDC_EDIT120, IDC_EDIT137, IDC_EDIT152 },
+	{ IDC_EDIT53, IDC_EDIT121, IDC_EDIT138, IDC_EDIT153 },
+	{ IDC_EDIT54, IDC_EDIT122, IDC_EDIT139, IDC_EDIT154 },
+	{ IDC_EDIT55, IDC_EDIT123, IDC_EDIT140, IDC_EDIT155 },
+	{ IDC_EDIT91, IDC_EDIT124, IDC_EDIT141, IDC_EDIT156 },
+	{ IDC_EDIT92, IDC_EDIT125, IDC_EDIT142, IDC_EDIT157 },
+	{ IDC_EDIT93, IDC_EDIT126, IDC_EDIT143, IDC_EDIT158 },
+	{ IDC_EDIT94, IDC_EDIT127, IDC_EDIT144, IDC_EDIT159 },
+	{ IDC_EDIT95, IDC_EDIT128, IDC_EDIT145, IDC_EDIT160 } };
+	int structure1[6] = { 14, 4, EDITBX, EDITBX, EDITNUM, EDITNUM };
+
+	vector<vector<int>> vvPara;
+	for (int i = 0; i < 18; i++) {
+		vector<int> vPara;
+		for (int j = 0; j < 4; j++)
+			vPara.push_back(parameters1[i][j]);
+		vvPara.push_back(vPara);
+	}
+	_vvvParameters.push_back(vvPara);
+
+	vector<int> vStr;
+	for (int i = 0; i < 6; i++) {
+		vStr.push_back(structure1[i]);
+	}
+	_vvSubformStructure.push_back(vStr);
+
+	_vHaveDataSubform.push_back(-1);
+
+	vStr.clear(); vStr.push_back(0); vStr.push_back(14); _vvSubformRecordRange.push_back(vStr);
 }
 
 CPersonalForm12::~CPersonalForm12()
 {
-	m_fontEdit.DeleteObject();
+	//m_fontEdit.DeleteObject();
 }
 
-void CPersonalForm12::SetCurrentFile(CString filePath)
-{
-	m_strCurrentFolder = filePath.Left(filePath.Find(_T("/"), 0));
-	m_strCurrentFile = filePath.Right(filePath.GetLength() - filePath.Find(_T("/"), 0) - 1);
-}
+//void CPersonalForm12::SetCurrentFile(CString filePath)
+//{
+//	m_strCurrentFolder = filePath.Left(filePath.Find(_T("/"), 0));
+//	m_strCurrentFile = filePath.Right(filePath.GetLength() - filePath.Find(_T("/"), 0) - 1);
+//}
 
 void CPersonalForm12::DoDataExchange(CDataExchange* pDX)
 {
@@ -65,6 +100,49 @@ void CPersonalForm12::Dump(CDumpContext& dc) const
 #endif //_DEBUG
 
 
+void CPersonalForm12::ShowEditbox(int nID, char *data)
+{
+	GetDlgItem(nID)->SetWindowTextW(CA2W(data, CP_UTF8));
+}
+
+void CPersonalForm12::ShowRadiobtn(int nWhich, char *data)
+{
+	int nSub = nWhich & 0xFF00; nSub >>= 8;
+	int nSWhich = nWhich & 0xF;
+
+}
+
+void CPersonalForm12::ShowDatapicker(int nID, char *data)
+{
+	COleDateTime t; t.ParseDateTime(CA2W(data, CP_UTF8));
+	((CDateTimeCtrl*)GetDlgItem(nID))->SetTime(t);
+}
+
+void CPersonalForm12::GetNumber(int nWhich, int &num)
+{
+	int nSub = nWhich & 0xFF00; nSub >>= 8;
+	int nSWhich = nWhich & 0xF;
+
+}
+
+void CPersonalForm12::GetString(int nID, CString &str)
+{
+	GetDlgItem(nID)->GetWindowTextW(str); str.Trim();
+}
+
+BOOL CPersonalForm12::hasData(int isub, int irow)
+{
+	CString strText;
+
+	if (isub == 1) {
+		vector<vector<int>> vvParam = _vvvParameters[isub - 1];
+		GetDlgItem(vvParam[irow][0])->GetWindowTextW(strText); strText.Trim();
+		if (strText.IsEmpty())
+			return FALSE;
+	}
+
+	return TRUE;
+}
 // CPersonalForm12 消息处理程序
 
 
@@ -172,6 +250,7 @@ void CPersonalForm12::OnBnClickedButtonCloseForm3()
 	CMainFrame* pWnd = (CMainFrame*)AfxGetApp()->m_pMainWnd;
 
 	::PostMessage(pWnd->m_hWnd, WM_SHOW_DEFAULT_SUMMARY, 0l, LPARAM(&m_strCurrentFolder));
+	::PostMessage(this->m_hWnd, WM_DESTROY, 0l, 0l);
 }
 
 
@@ -180,6 +259,7 @@ void CPersonalForm12::OnInitialUpdate()
 	CFormView::OnInitialUpdate();
 
 	// TODO:  在此添加专用代码和/或调用基类
+#if 0
 	stringstream ss;
 	ss << "select file_id from orgnization_file where file_name='" << CW2A(m_strCurrentFile.GetBuffer(), CP_UTF8) << "' and folder_name='" <<
 		CW2A(m_strCurrentFolder.GetBuffer(), CP_UTF8) << "';";
@@ -242,9 +322,49 @@ void CPersonalForm12::OnInitialUpdate()
 	GetDlgItem(IDC_EDIT161)->SetWindowTextW(str);
 	help->closeDB();
 	delete help;
+#endif
+
+	((CButton*)GetDlgItem(IDC_BUTTON_CLOSE_FORM3))->SetBitmap(m_bmpClose);
+	DoShowForm();
+
+	BOOL hasData = FALSE;
+	vector<int>::iterator itHas = _vHaveDataSubform.begin();
+	while (itHas != _vHaveDataSubform.end()) {
+		if (*itHas != -1) {
+			hasData = TRUE; break;
+		}
+		itHas++;
+	}
+	if (hasData) {
+		GetDlgItem(IDC_CMD_SAVE_FORM)->ShowWindow(SW_HIDE);
+		GetDlgItem(IDC_CMD_UPDATE_FORM3)->ShowWindow(SW_SHOW);
+	}
+
+	stringstream ss;
+	ss << "select file_id from orgnization_file where file_name='" << CW2A(m_strCurrentFile.GetBuffer(), CP_UTF8) << "' and folder_name='" <<
+		CW2A(m_strCurrentFolder.GetBuffer(), CP_UTF8) << "';";
+	CSQLiteHelper *help = new CSQLiteHelper();
+	help->openDB("kiwi.db3");
+	int row, col;
+	char *eee = "i"; char **result = &eee;
+	char **re = help->rawQuery(ss.str().c_str(), &row, &col, result);
+	int file_id = atoi(re[1 * col + 0]);
+
+	ss.str(""); ss.clear();
+	ss << "select file_17IfHaveThisSituation from file_form_flags where file_id=" << file_id << ";";
+	re = help->rawQuery(ss.str().c_str(), &row, &col, result);
+	if (row < 1) {
+		m_Radio12_2_0 = -1;
+	}
+	else
+		m_Radio12_2_0 = 1 - atoi(re[1 * col + 0]);  //分组的原因，使得要用1-
+	help->closeDB(); delete help;
+
+	UpdateData(FALSE);
 }
 
 void CPersonalForm12::OnBnClickedCmdUpdateForm()
 {
 	// TODO:  在此添加控件通知处理程序代码
+	DoUpdateForm();
 }
