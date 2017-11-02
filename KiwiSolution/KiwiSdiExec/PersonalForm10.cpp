8776be622f8e8882bd922ebde520a154d4eefbf8
Shawn Cheng <shawncheng@163.com>
2017-11-02 20:24:47 +0800

    bug fixed

--- KiwiSolution/KiwiSdiExec/PersonalForm10.cpp
+++ KiwiSolution/KiwiSdiExec/PersonalForm10.cpp
@@ -21,6 +21,7 @@ CPersonalForm10::CPersonalForm10()
 {
 	//LOGFONT lf; memset(&lf, 0, sizeof(LOGFONT)); lf.lfHeight = 25;  _tcsncpy_s(lf.lfFaceName, LF_FACESIZE, _T("仿宋体"), 3); lf.lfWeight = 400;
 	//m_fontEdit.CreateFontIndirect(&lf);
+	m_FormID = 10;
 	int parameters1[18][4] = { { IDC_EDIT47, IDC_EDIT98, IDC_EDIT97, IDC_EDIT99 },
 	{ IDC_EDIT48, IDC_EDIT102, IDC_EDIT133, IDC_EDIT110 },
 	{ IDC_EDIT49, IDC_EDIT103, IDC_EDIT134, IDC_EDIT111 },
@@ -55,6 +56,8 @@ CPersonalForm10::CPersonalForm10()
 		vStr.push_back(structure1[i]);
 	}
 	_vvSubformStructure.push_back(vStr);
+
+	_vHaveDataSubform.push_back(-1);
 
 	vStr.clear(); vStr.push_back(0); vStr.push_back(18); _vvSubformRecordRange.push_back(vStr);
 }
@@ -332,6 +335,27 @@ void CPersonalForm10::OnInitialUpdate()
 		GetDlgItem(IDC_CMD_UPDATE_FORM3)->ShowWindow(SW_SHOW);
 	}
 
+	stringstream ss;
+	ss << "select file_id from orgnization_file where file_name='" << CW2A(m_strCurrentFile.GetBuffer(), CP_UTF8) << "' and folder_name='" <<
+		CW2A(m_strCurrentFolder.GetBuffer(), CP_UTF8) << "';";
+	CSQLiteHelper *help = new CSQLiteHelper();
+	help->openDB("kiwi.db3");
+	int row, col;
+	char *eee = "i"; char **result = &eee;
+	char **re = help->rawQuery(ss.str().c_str(), &row, &col, result);
+	int file_id = atoi(re[1 * col + 0]);
+
+	ss.str(""); ss.clear();
+	ss << "select file_17IfHaveThisSituation from file_form_flags where file_id=" << file_id << ";";
+	re = help->rawQuery(ss.str().c_str(), &row, &col, result);
+	if (row < 1) {
+		m_Radio12_1_0 = -1;
+	}
+	else
+		m_Radio12_1_0 = 1 - atoi(re[1 * col + 0]);  //分组的原因，使得要用1-
+	help->closeDB(); delete help;
+
+	UpdateData(FALSE);
 }
 
 void CPersonalForm10::OnBnClickedCmdUpdateForm()
