2059a95076f43b523710f1689bf2bc44f90028df
Shawn Cheng <shawncheng@163.com>
2017-11-04 16:57:01 +0800

    组织pane新增当前选择提示

--- KiwiSolution/KiwiSdiExec/resource.h
+++ KiwiSolution/KiwiSdiExec/resource.h
@@ -822,6 +822,7 @@
 #define IDC_EDIT_NEW_USER_PWD           1516
 #define IDC_EDIT_NEW_USER_PWD2          1517
 #define IDC_EDIT_NAME_BACKUP            1517
+#define IDC_STATIC_CURSELECTION         1518
 #define ID_32771                        32771
 #define ID_TOOL_FULLSCREEN              32772
 #define ID_Menu                         32773
@@ -839,7 +840,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        358
 #define _APS_NEXT_COMMAND_VALUE         32781
-#define _APS_NEXT_CONTROL_VALUE         1518
+#define _APS_NEXT_CONTROL_VALUE         1519
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
